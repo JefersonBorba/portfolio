1ceb93e569f46f01f1d42a80f4cdead8fd7dcde9
Jeferson Borba <jeff@pop-os.localdomain>
2021-02-04 20:33:37 -0300

    temporary HardSkills implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Container } from "./style"
 import Header from "./components/Header"
 import About from "./components/About"
+import HardSkills from "./components/HardSkills"
 
 const App = () => {
   return (
     <Container className="App">
       <Header />
       <About />
+      <HardSkills />
     </Container>
   );
 }
---
+++ src/components/HardSkills/index.jsx
@@ -0,0 +1,74 @@
+import {Container, Skill, Title, SkillHolder} from "./style"
+
+const HardSkills = () => {
+    return (
+        <Container>
+            <Title>HardSkills</Title>
+            <SkillHolder>
+                <Skill>
+                    <p>HTML</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-half"></i>
+                    </div>
+                    
+                </Skill>
+                <Skill>
+                    <p>CSS</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-half"></i>
+                    </div>
+                </Skill>
+                <Skill>
+                    <p>JS</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-transparent"></i>
+                    </div>
+                </Skill>
+                <Skill>
+                    <p>React</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-transparent"></i>
+                    </div>
+                </Skill>
+                <Skill>
+                    <p>Git</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-half"></i>
+                        <i class="nes-icon is-small star is-transparent"></i>
+                    </div>
+                </Skill>
+                <Skill>
+                    <p>Python</p>
+                    <div>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star"></i>
+                        <i class="nes-icon is-small star is-half"></i>
+                        <i class="nes-icon is-small star is-transparent"></i>
+                    </div>
+                </Skill>
+            </SkillHolder>
+        </Container>
+    )
+}
+
+export default HardSkills;
